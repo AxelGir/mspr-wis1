faff158de7269ea6f358b01de762a93f0ec8ce76
Axel <axel.giraudeau@yahoo.com>
2021-02-01 16:47:53 +0100

    update: main

---
+++ layouts/footer.php
@@ -0,0 +1,31 @@
+<footer>
+    <section>
+
+        <div class="container">
+            <div class="content">
+                <span class="test-border">Nos Réseaux Sociaux</span>
+                <a href="#">
+                    <img src="images/001-facebook.png" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/002-twitter.png" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/003-instagram.png" alt="">
+                </a>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="container">
+
+        <div class="content">
+            <a href="#">Politique de confidentialité</a>
+            <a href="#">Mentions légales</a>
+        </div>
+
+    </section>
+</footer>
+</body>
+</html>
