3a1e23786bfa74c1c711e242f66079827bf04af1
Axel <axel.giraudeau@yahoo.com>
2021-02-02 10:11:39 +0100

    update:création de l'accueil

--- index.php
+++ index.php
@@ -3,7 +3,32 @@
     <main id="main">
         <section class="container">
             <div class="content">
-                MAIN - BODY
+                <h2 class="home-title">- Notre concept -</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
+                    dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
+                    mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos ducimus
+                    ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
+                <a class="know-more" href="#">En savoir plus...</a>
+            </div>
+        </section>
+
+        <section class="container">
+            <div class="content">
+                <h2 class="home-title">- On vous présente nos gourdes -</h2>
+            </div>
+            <div class="cards-wrapper">
+
+            </div>
+        </section>
+
+        <section class="container">
+            <div class="content">
+                <h2 class="home-title">- Des détails sur nous -</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
+                    dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
+                    mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos ducimus
+                    ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
+                <a class="know-more" href="#">En savoir plus...</a>
             </div>
         </section>
     </main>
