374ac25326ad2f606877d650f6621d6dbfdcd920
Axel <axel.giraudeau@yahoo.com>
2021-02-02 11:17:59 +0100

    update: virer les .content

--- index.php
+++ index.php
@@ -3,23 +3,19 @@
     <main id="main">
         <section id="home-concept">
             <div class="container">
-                <div class="content">
-                    <h2 class="home-title">- Notre concept -</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
-                        dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
-                        mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos
-                        ducimus
-                        ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
-                    <a class="know-more" href="#">En savoir plus...</a>
-                </div>
+                <h2 class="home-title">- Notre concept -</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
+                    dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
+                    mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos
+                    ducimus
+                    ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
+                <a class="know-more" href="#">En savoir plus...</a>
             </div>
         </section>
 
         <section id="home-gourdes">
             <div class="container">
-                <div class="content">
-                    <h2 class="home-title">- On vous présente nos gourdes -</h2>
-                </div>
+                <h2 class="home-title">- On vous présente nos gourdes -</h2>
                 <div class="cards-wrapper">
                     <div class="card">
                         <img src="https://placehold.it/100"
@@ -46,9 +42,7 @@
 
         <section id="home-tarifs">
             <div class="container">
-                <div class="content">
-                    <h2 class="home-title">- Nos différents tarifs -</h2>
-                </div>
+                <h2 class="home-title">- Nos différents tarifs -</h2>
                 <div class="cards-wrapper">
                     <div class="card">
                         <img src="https://placehold.it/100"
@@ -72,7 +66,6 @@
 
         <section id="home-a-propos">
             <div class="container">
-                <div class="content">
                     <h2 class="home-title">- Des détails sur nous -</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
                         dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
@@ -81,20 +74,18 @@
                         ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
                     <a class="know-more" href="#">En savoir plus...</a>
                 </div>
-            </div>
         </section>
 
         <section id="home-contact">
             <div class="container">
-                <div class="content">
 
                     <a class="jsp-encore" href="#">
                         <h2>- Nous contacter -</h2>
                     </a>
 
                 </div>
-            </div>
         </section>
+
     </main>
 
 <?php require_once 'layouts/footer.php' ?>
