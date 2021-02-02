41609dd37b9899ea4321937417319de65215def9
Axel <axel.giraudeau@yahoo.com>
2021-02-02 10:48:31 +0100

    update: réparation

--- index.php
+++ index.php
@@ -1,38 +1,72 @@
 <?php require_once 'layouts/header.php' ?>
 
     <main id="main">
-        <section class="container">
-            <div class="content">
-                <h2 class="home-title">- Notre concept -</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
-                    dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
-                    mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos ducimus
-                    ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
-                <a class="know-more" href="#">En savoir plus...</a>
+        <section id="home-concept">
+            <div class="container">
+                <div class="content">
+                    <h2 class="home-title">- Notre concept -</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur cum debitis delectus,
+                        dolores earum, explicabo facilis fuga fugiat illo impedit in laboriosam laborum laudantium minus
+                        mollitia numquam odit possimus quae, rem saepe sunt tempora ullam! Accusantium, dignissimos
+                        ducimus
+                        ipsam molestias odit quae veniam? A asperiores consequuntur inventore maiores provident!</p>
+                    <a class="know-more" href="#">En savoir plus...</a>
+                </div>
             </div>
         </section>
 
-        <section class="container">
-            <div class="content">
-                <h2 class="home-title">- On vous présente nos gourdes -</h2>
-            </div>
-            <div class="cards-wrapper">
-                <div class="card">
-                    <img src="https://placehold.it/100"
-                         alt="gourde-en-plastique">
-                    <a class="tarifs-btn" href="#">Gourdes en plastique</a>
+        <section id="home-gourdes">
+            <div class="container">
+                <div class="content">
+                    <h2 class="home-title">- On vous présente nos gourdes -</h2>
                 </div>
-                <div class="card">
-                    <img src="https://placehold.it/100"
-                         alt="gourde-en-verre">
-                    <a class="tarifs-btn" href="#">Gourdes en verre</a>
-                </div>
-                <div class="card">
-                    <img src="https://placehold.it/100"
-                         alt="gourde-en-metal">
-                    <a class="tarifs-btn" href="#">Gourdes en métal</a>
-                </div>
+                <div class="cards-wrapper">
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-plastique">
+                        <h3>Nos gourdes en plastique</h3>
+                        <a class="know-more" href="#">En savoir plus</a>
+                    </div>
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-verre">
+                        <h3>Nos gourdes en verre</h3>
+                        <a class="know-more" href="#">En savoir plus</a>
+                    </div>
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-metal">
+                        <h3>Nos gourdes en métal</h3>
+                        <a class="know-more" href="#">En savoir plus</a>
+                    </div>
 
+                </div>
+            </div>
+        </section>
+
+        <section id="home-tarifs">
+            <div class="container">
+                <div class="content">
+                    <h2 class="home-title">- Nos différents tarifs -</h2>
+                </div>
+                <div class="cards-wrapper">
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-plastique">
+                        <a class="tarifs-btn" href="#">Gourdes en plastique</a>
+                    </div>
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-verre">
+                        <a class="tarifs-btn" href="#">Gourdes en verre</a>
+                    </div>
+                    <div class="card">
+                        <img src="https://placehold.it/100"
+                             alt="gourde-en-metal">
+                        <a class="tarifs-btn" href="#">Gourdes en métal</a>
+                    </div>
+
+                </div>
             </div>
         </section>
 
