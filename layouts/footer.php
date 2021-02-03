671403d39ef3064a90f46cfe41183c293bc1fe02
Axel <axel.giraudeau@yahoo.com>
2021-02-03 09:56:57 +0100

    update: copyrights

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,31 +1,35 @@
 <footer id="footer">
-    <section id="foot-left">
 
+    <div class="foot-left">
         <div class="container">
             <div class="content">
-
-                    <a href="https://facebook.com">
-                        <img src="images/001-facebook.png" alt="">
-                    </a>
-                    <a href="https://twitter.com">
-                        <img src="images/002-twitter.png" alt="">
-                    </a>
-                    <a href="https://instagram.com">
-                        <img src="images/003-instagram.png" alt="">
-                    </a>
-                </div>
+                <a href="https://facebook.com">
+                    <img src="images/001-facebook.png" alt="">
+                </a>
+                <a href="https://twitter.com">
+                    <img src="images/002-twitter.png" alt="">
+                </a>
+                <a href="https://instagram.com">
+                    <img src="images/003-instagram.png" alt="">
+                </a>
             </div>
+        </div>
+    </div>
 
-    </section>
+    <div class="foot-mid">
+        <div class="container">
+<span class="copyrights">Copyrights - Dewi et Axel</span>
+        </div>
+    </div>
 
-    <section id="foot-right">
 
-        <div class="content">
+    <div class="foot-right">
+        <div class="container">
             <a class="sous-titre" href="#">Politique de confidentialité</a>
             <a class="sous-titre" href="#">Mentions légales</a>
         </div>
+    </div>
 
-    </section>
 </footer>
 
 <script src="assets/js/app.js"></script>
