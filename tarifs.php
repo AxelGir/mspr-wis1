4ebcf5828aa5fa3f86c1b1f9a3db77f16ddfb53b
Dewi <dewitcrel@gmail.Com>
2021-02-04 15:15:16 +0100

    update: tarif css

--- tarifs.php
+++ tarifs.php
@@ -8,10 +8,12 @@ if ($_POST) {
         endif;
         return $value;
     }
+
     function getRealPrice($price = 0, $tva = 0.2)
     {
         return $price + ($price * $tva);
     }
+
     $price = getValueFromField('price');
     $size = getValueFromField('size');
     $price_ttc = getRealPrice($price, $size);
@@ -23,43 +25,52 @@ if ($_POST) {
 <main id="main">
     <section id="tarifs-top">
         <div class="container">
-        <h2 class="home-title">- Nos tarifs -</h2>
+            <h2 class="home-title">- Nos tarifs -</h2>
             <p>Selectionnez un modèle et une taille</p>
         </div>
     </section>
     <section id="tarifs-selector">
+        <div class="container">
         <div class="selector-gourd">
             <form action="" method="post">
-            <div class="container-img">
-                <img src="http://placehold.it/200" alt="Gourde en plastique">
-                <input type="radio" id="plastique" name="img-select" checked>
-                <img src="http://placehold.it/200" alt="Gourde en verre">
-                <input type="radio" id="verre" name="img-select" checked>
-                <img src="http://placehold.it/200" alt="Gourde en métal">
-                <input type="radio" id="metal" name="img-select" checked>
-            </div>
-            <div class="selector-size">
-                <label for="size">Taille</label>
+
+                <div class="container-img d-flex">
+                    <div class="form-group">
+                        <input type="radio" id="plastique" name="img-select">
+                        <label for="plastique">
+                            <img src="http://placehold.it/200" alt="Gourde en plastique">
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <input type="radio" id="verre" name="img-select">
+                        <label for="verre">
+                            <img src="http://placehold.it/200" alt="Gourde en verre">
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <input type="radio" id="metal" name="img-select">
+                        <label for="metal">
+                            <img src="http://placehold.it/200" alt="Gourde en métal">
+                        </label>
+                    </div>
+                </div>
+
+                <div class="selector-size">
+                    <label for="size">Taille</label>
                     <select name="size" id="size" required>
-                        <?php if (isset($size) && $size === '0.5'): ?>
-                        <option value="0.5" selected>0,5L</option>
-                        <?php else: ?>
-                        <option value="0.5">0.5L</option>
-                        <?php if (isset($size) && $size === '1'): ?>
-                        <option value="1" selected>1L</option>
-                        <?php else: ?>
-                        <option value="1">1L</option>
-                        <?php if (isset($size) && $size === '2'): ?>
-                        <option value="2" selected>2L</option>
-                        <?php else: ?>
-                        <option value="2">2L</option>
-                        <?php endif; ?>
+
+                        <option value="0.5" <?php echo isset($size) && $size === '0.5' ? 'selected' : null; ?>>0,5L
+                        </option>
+
+                        <option value="1" <?php echo isset($size) && $size === '1' ? 'selected' : null ?>>1L</option>
+
+                        <option value="2" <?php echo isset($size) && $size === '2' ? 'selected' : null ?>>2L</option>
+
                     </select>
                     <button class="btn btn-know-more">Générer ma commande</button>
-            </div>
+                </div>
         </div>
-        <?php endif; ?>
-        <?php endif; ?>
+        </div>
     </section>
 
 </main>
