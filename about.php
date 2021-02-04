2b1a4e9cc7b73b9a3de03824b32907fab05ae041
Axel <axel.giraudeau@yahoo.com>
2021-02-04 11:40:00 +0100

    update: css refresh page about

--- about.php
+++ about.php
@@ -5,17 +5,29 @@
         <section id="about-top">
             <div class="container">
                 <h2 class="home-title">- A PROPOS DE NOUS -</h2>
+
+                <p>Concernant l'origine de <b>Avant-Gourde</b>, Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aperiam culpa dicta dolore
+                    dolorum ducimus ea eveniet harum impedit magni mollitia, optio perspiciatis provident quae qui
+                    quibusdam repellat sapiente sed sequi sint unde ut vero! Est officiis, vitae! Ab dicta facilis iste
+                    itaque laboriosam nam, odio porro ut voluptates?</p>
+
+                <h3 class="home-title">- ET CONCERNANT L'ÉQUIPE -</h3>
+
                 <div class="cards-wrapper">
 
                     <div class="card">
                         <img src="images/myAvatar.png" alt="photo de Axel">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque fugiat mollitia
-                            nam nostrum quaerat!</p>
+                        <p>A titre personnel, il est clair que je préfère les gourdes en <b>VERRE</b>, n'altérant pas le
+                            goût
+                            de la boisson que l'on met à l'intérieur. Seuls points négatifs : elle est fragile et la
+                            température de la boisson se ressent légèrement à l'extérieur de la gourde.</p>
                     </div>
                     <div class="card">
                         <img src="images/DewiAvatar.png" alt="photo de Dewi">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque fugiat mollitia
-                            nam nostrum quaerat!</p>
+                        <p>Pour ma part, ma gourde préferée est celle en <b>PLASTIQUE</b>. Elle peut s'emporter
+                            facilement
+                            partout, n'est pas du tout fragile et ne fait pas de bruit en cas de chute au sol. En plus,
+                            on ne ressent que très peu la température à travers les parois.</p>
                     </div>
 
                 </div>
