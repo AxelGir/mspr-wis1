efdff90039ceb8f04873a16f3e4d51f84740db34
Axel <axel.giraudeau@yahoo.com>
2021-02-04 16:27:17 +0100

    update: page 404 finish

--- 404-page.php
+++ 404-page.php
@@ -1,1 +1,21 @@
-coucou
+<?php require_once "layouts/header.php"?>
+
+<section id="not-found">
+    <div id="title">Simple Pure CSS3 &bull; 404 Error Page</div>
+    <div class="circles">
+        <p>404<br>
+            <small>Vous avez perdu votre gourde ?</small>
+        </p>
+        <span class="circle big"></span>
+        <span class="circle med"></span>
+        <span class="circle small"></span>
+    </div>
+</section>
+
+<section id="back">
+    <div class="container">
+        <a class="btn btn-tarifs btn-back" href="index.php">Revenez à l'accueil !</a>
+    </div>
+</section>
+
+<?php require_once "layouts/footer.php"?>
